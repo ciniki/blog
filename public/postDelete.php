1243daf504319000b2d6c4e0d2cb5fb5378f956b
Andrew Rivett <veggiefrog@gmail.com>
2014-11-10 10:26:22 -0500

    Fixed bug with webCollectionDeleteObjRef hook

--- public/postDelete.php
+++ public/postDelete.php
@@ -181,7 +181,7 @@ function ciniki_blog_postDelete(&$ciniki) {
 		&& ($ciniki['business']['modules']['ciniki.web']['flags']&0x08) == 0x08
 		) {
 		ciniki_core_loadMethod($ciniki, 'ciniki', 'web', 'hooks', 'webCollectionDeleteObjRef');
-		$rc = ciniki_web_hooks_collectionDeleteObjRef($ciniki, $args['business_id'],
+		$rc = ciniki_web_hooks_webCollectionDeleteObjRef($ciniki, $args['business_id'],
 			array('object'=>'ciniki.blog.post', 'object_id'=>$args['post_id']));
 		if( $rc['stat'] != 'ok' ) {	
 			ciniki_core_dbTransactionRollback($ciniki, 'ciniki.blog');
