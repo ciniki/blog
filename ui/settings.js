b2cb91297ac546c1f2c4b23fd5ec18523c610009
Andrew Rivett <veggiefrog@gmail.com>
2016-06-25 13:36:07 -0400

    Updated settings to new UI format

--- ui/settings.js
+++ ui/settings.js
@@ -2,32 +2,55 @@
 function ciniki_blog_settings() {
     this.toggleOptions = {'no':' No ', 'yes':' Yes '};
 
-    this.init = function() {
-        //
-        // The main panel, which lists the options for production
-        //
-        this.main = new M.panel('Blog Settings',
-            'ciniki_blog_settings', 'main',
-            'mc', 'medium', 'sectioned', 'ciniki.blog.settings.main');
-        this.main.sections = {
-            'options':{'label':'', 'fields':{
-                'mailing-subject-prepend':{'label':'Mailing Subject', 'type':'text'},
-            }},
-        };
-        this.main.fieldValue = function(s, i, d) { 
-            return this.data[i];
-        };
+    //
+    // The main panel, which lists the options for production
+    //
+    this.main = new M.panel('Blog Settings', 'ciniki_blog_settings', 'main', 'mc', 'medium', 'sectioned', 'ciniki.blog.settings.main');
+    this.main.sections = {
+        'options':{'label':'', 'fields':{
+            'mailing-subject-prepend':{'label':'Mailing Subject', 'type':'text'},
+        }},
+    };
+    this.main.fieldValue = function(s, i, d) { 
+        return this.data[i];
+    };
 
-        //  
-        // Callback for the field history
-        //  
-        this.main.fieldHistoryArgs = function(s, i) {
-            return {'method':'ciniki.blog.settingsHistory', 'args':{'business_id':M.curBusinessID, 'field':i}};
-        };
-
-        this.main.addButton('save', 'Save', 'M.ciniki_blog_settings.saveSettings();');
-        this.main.addClose('Cancel');
+    //  
+    // Callback for the field history
+    //  
+    this.main.fieldHistoryArgs = function(s, i) {
+        return {'method':'ciniki.blog.settingsHistory', 'args':{'business_id':M.curBusinessID, 'field':i}};
+    };
+    this.main.open = function(cb) {
+        var rsp = M.api.getJSONCb('ciniki.blog.settingsGet', 
+            {'business_id':M.curBusinessID}, function(rsp) {
+                if( rsp.stat != 'ok' ) {
+                    M.api.err(rsp);
+                    return false;
+                }
+                var p = M.ciniki_blog_settings.main;
+                p.data = rsp.settings;
+                p.refresh();
+                p.show(cb);
+            });
     }
+    this.main.save = function() {
+        var c = this.main.serializeForm('no');
+        if( c != '' ) {
+            var rsp = M.api.postJSONCb('ciniki.blog.settingsUpdate', 
+                {'business_id':M.curBusinessID}, c, function(rsp) {
+                    if( rsp.stat != 'ok' ) {
+                        M.api.err(rsp);
+                        return false;
+                    } 
+                    M.ciniki_blog_settings.main.close();
+                });
+        } else {
+            M.ciniki_blog_settings.main.close();
+        }
+    }
+    this.main.addButton('save', 'Save', 'M.ciniki_blog_settings.main.save();');
+    this.main.addClose('Cancel');
 
     //
     // Arguments:
@@ -47,39 +70,6 @@ function ciniki_blog_settings() {
             return false;
         } 
 
-        this.showMain(cb);
-    }
-
-    //
-    // Grab the stats for the business from the database and present the list of orders.
-    //
-    this.showMain = function(cb) {
-        var rsp = M.api.getJSONCb('ciniki.blog.settingsGet', 
-            {'business_id':M.curBusinessID}, function(rsp) {
-                if( rsp.stat != 'ok' ) {
-                    M.api.err(rsp);
-                    return false;
-                }
-                var p = M.ciniki_blog_settings.main;
-                p.data = rsp.settings;
-                p.refresh();
-                p.show(cb);
-            });
-    }
-
-    this.saveSettings = function() {
-        var c = this.main.serializeForm('no');
-        if( c != '' ) {
-            var rsp = M.api.postJSONCb('ciniki.blog.settingsUpdate', 
-                {'business_id':M.curBusinessID}, c, function(rsp) {
-                    if( rsp.stat != 'ok' ) {
-                        M.api.err(rsp);
-                        return false;
-                    } 
-                    M.ciniki_blog_settings.main.close();
-                });
-        } else {
-            M.ciniki_blog_settings.main.close();
-        }
+        this.main.open(cb);
     }
 }
