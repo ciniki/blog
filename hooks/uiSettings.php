f887427dad7660ba29da8c5ab889e4628b73d813
Andrew Rivett <veggiefrog@gmail.com>
2016-09-25 15:55:10 -0400

    Updated uiSettings for settings_menu_items

--- hooks/uiSettings.php
+++ hooks/uiSettings.php
@@ -17,7 +17,7 @@ function ciniki_blog_hooks_uiSettings($ciniki, $business_id, $args) {
     //
     // Setup the default response
     //
-    $rsp = array('stat'=>'ok', 'menu_items'=>array());  
+    $rsp = array('stat'=>'ok', 'menu_items'=>array(), 'settings_menu_items'=>array());
 
     //
     // Check permissions for what menu items should be available
@@ -53,6 +53,16 @@ function ciniki_blog_hooks_uiSettings($ciniki, $business_id, $args) {
             );
         $rsp['menu_items'][] = $menu_item;
     } 
+
+    if( isset($ciniki['business']['modules']['ciniki.blog']) && isset($ciniki['business']['modules']['ciniki.mail']) 
+        && (isset($args['permissions']['owners'])
+            || isset($args['permissions']['employees'])
+            || isset($args['permissions']['resellers'])
+            || ($ciniki['session']['user']['perms']&0x01) == 0x01
+            )
+        ) {
+        $rsp['settings_menu_items'][] = array('priority'=>3600, 'label'=>'Blog', 'edit'=>array('app'=>'ciniki.blog.settings'));
+    }
 
     return $rsp;
 }
